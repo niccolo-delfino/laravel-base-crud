427646cb890e88ab68291dfcbc20e2a44db42073
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-18 17:12:34 +0100

    wip redirect

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -49,6 +49,8 @@ class MovieController extends Controller
       $movieNew->protagonista = $data['protagonista'];
 
       $movieNew->save();
+
+      return redirect()->route('movies.index');
     }
 
     /**
