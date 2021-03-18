75f1354b474990e89753eff560c23b5f2b1d624e
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-18 16:50:16 +0100

    wip csrf

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -1,8 +1,26 @@
 @extends('layouts.app')
-@section('title','film')
+@section('title','gestione film')
 
 @section('content')
 <h1>Inserisci un film</h1>
 
-
+<div class="container">
+  <form method="post">
+    @method('POST')
+    @csrf
+  <div class="form-group">
+    <label for="inputFilm">Titolo</label>
+    <input type="text" class="form-control" name="titolo" id="inputFilm">
+  </div>
+  <div class="form-group">
+    <label for="inputRegista">Regista</label>
+    <input type="text" class="form-control" name="regista" id="inputRegista">
+  </div>
+  <div class="form-group">
+    <label for="inputProtagonista">Protagonista</label>
+    <input type="text" class="form-control" name="protagonista" id="inputProtagonista">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
 @endsection
