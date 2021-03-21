7577764c5b36e70c5a8770897f6f8d5f86965ca6
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-21 21:18:47 +0100

    wip value

--- resources/views/movies/edit.blade.php
+++ resources/views/movies/edit.blade.php
@@ -11,15 +11,15 @@
     @csrf
   <div class="form-group">
     <label for="inputFilm">Titolo</label>
-    <input type="text" class="form-control" name="titolo" id="inputFilm">
+    <input type="text" class="form-control" name="titolo" id="inputFilm" value="{{$video->titolo}}">
   </div>
   <div class="form-group">
     <label for="inputRegista">Regista</label>
-    <input type="text" class="form-control" name="regista" id="inputRegista">
+    <input type="text" class="form-control" name="regista" id="inputRegista" value="{{$video->regista}}">
   </div>
   <div class="form-group">
     <label for="inputProtagonista">Protagonista</label>
-    <input type="text" class="form-control" name="protagonista" id="inputProtagonista">
+    <input type="text" class="form-control" name="protagonista" id="inputProtagonista" value="{{$video->protagonista}}">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
